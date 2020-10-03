9223aadb8cc623083fa487a34ef22e5120dae5df
Claudio Capobianco <claudio.capobianco@marconicloud.it>
2020-10-03 07:47:50 +0000

    Add encapsulation for "nome"

--- Calciatore.java
+++ Calciatore.java
@@ -1,14 +1,31 @@
 // Il nome della classe principale deve essere uguale a quella del file in cui si trova
 class Calciatore {
-  String nome = "";
-  String ruolo = "";
+
+  // "private" è un modificatore che fa in modo che dall'esterno di questo file non possa essere modificata la variabile
+
+  // Questa pratica si chiama INCAPSULAMENTO ("encapsulation")
+
+  private String nome = "";
+  private String ruolo = "";
   
   // Statistiche
   // Tutte le stats sono da 1 a 100
-  int velocita = 0;
-  int dribbling = 0;
-  int tiro = 0;
-  int difesa = 0;
+  private int velocita = 0;
+  private int dribbling = 0;
+  private int tiro = 0;
+  private int difesa = 0;
     
   String squadra = "";
+
+  // Costruttore
+  public Calciatore(String nome) {
+    this.nome = nome;
+  }
+
+  // Funzione di getter
+  // Il gettere deve chiamarsi getNomevariabile
+  public String getNome() {
+    return this.nome;
+  }
+
 }
