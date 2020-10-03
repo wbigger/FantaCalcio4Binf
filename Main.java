a2dfaf0a94d6996fd7fc6ec41022e40cac7f16aa
Claudio Capobianco <claudio.capobianco@marconicloud.it>
2020-10-03 06:30:03 +0000

    Change welcome message

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Benvenuti al Fanta Calcio del 4Binf del Marconi di Civitavecchia!");
   }
 }
